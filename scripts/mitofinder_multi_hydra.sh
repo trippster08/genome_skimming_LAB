e0cb4c23f44a9618c00b4ae1f38968241c02ce5a
Tripp Macdonald <trippster08@outlook.com>
2023-02-06 15:39:57 -0500

    Changed output directory
    Mitofinder defaults to put the output in the same folder from which the program was run, and there doesn't appear to be a way to change this. So, I added "-wd", which tells hydra to run the program in whatever directory is listed, in this case mitofinder/results/. Since this would also cause the log files to be placed here, I have also changed the log output "-o" to jobs/logs/

--- scripts/mitofinder_multi_hydra.sh
+++ scripts/mitofinder_multi_hydra.sh
@@ -2,17 +2,14 @@
 
 path="$1"
 reference="$2"
-repath=`echo ${path} | cut -d "/" -f 1,2,3,4,5,6`
-
+repath=`echo ${path} | cut -d "/" -f 1,2,3,4,5`
+mkdir -p ${repath}/data/results/mitofinder
 
  for x in ${path}/*_R1_PE_trimmed.fastq.gz ; do 
-    sample=`echo ${x} | cut -d "_" -f 1,2,3,4 | cut -d "/" -f 8`
+    sample=`echo ${x} | cut -d "_" -f 1,2,3,4,5 | cut -d "/" -f 8`
 
-    qsub -o logs/${sample}_mitofinder.log \
+    qsub -o ${repath}/jobs/logs/${sample}_mitofinder.log \
+    -wd ${repath}/data/results/mitofinder \
     -N ${sample}_mitofinder \
     mitofinder_multi.job ${path} ${sample} ${reference}
-
-echo ${path}
-echo ${sample}
 done
-
