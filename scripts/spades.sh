c6a538d23cf9a9cef45dccf0af499022d6da9678
Tripp Macdonald <trippster08@outlook.com>
2023-05-04 16:05:19 -0400

    Revamp spades.sh
    This is a wholesale change of the spades shell script.

--- scripts/spades.sh
+++ scripts/spades.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-path="$1"
-repath=`echo ${path} | cut -d "/" -f 1,2,3,4,5,6`
-mkdir -p ${repath}/results/spades
+trimmed="$1"
+data=${trimmed}/../
+mkdir -p ${data}/results/spades
 
-for x in ${path}/*_R1_PE_trimmed.fastq.gz ; do 
-    sample=`echo ${x} | cut -d "_" -f 1,2,3,4 | cut -d "/" -f 8`
+ls ${trimmed}/*.fastq.gz &> /dev/null  || echo "Correct path to read files not entered (*.fastq.gz)"
+
+for x in ${trimmed}/*_R1_PE_trimmed.fastq.gz ; do 
+  sample=`basename ${x}`
+  name=`echo ${sample%_R*}`
+  echo $sample
+  echo $name
+  
+    qsub -o logs/${name}_spades.log \
+    -N ${name}_spades \
+    spades.job ${trimmed} ${name} ${data}
     
-    qsub -o logs/${sample}_spades.log \
-    -N ${sample}_spades \
-    spades_multi.job ${path} ${sample} ${repath}
-echo ${path}
-echo ${sample}
-echo ${repath}
 done
