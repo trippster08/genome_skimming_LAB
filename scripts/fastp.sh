4c559796cd4ba723b5924b1f4723fa79af0e4afd
Tripp Macdonald <trippster08@outlook.com>
2023-05-04 16:06:43 -0400

    Create fastp.sh
    This is a shell script to submit multiple read file to Hydra for quality and adapter trimming. fastp replaces trimmomatic.

---
+++ scripts/fastp.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+raw="$1"
+data=${raw}/../
+mkdir -p ${data}/trimmed_sequences
+ls ${raw}/*.fastq.gz &> /dev/null  || echo "Correct path to read files not entered (*.fastq.gz)"
+
+for x in ${raw}/*R1_001.fastq.gz ; do 
+  sample=`basename ${x}`
+  name=`echo ${sample%_S*}`
+  nameplus=`echo ${sample%_R*}`
+  echo $sample
+  echo $name
+  echo $nameplus
+
+    qsub -o logs/${name}_fastp.log \
+    -N ${name}_fastp \
+    fastp.job ${raw} ${data} ${name} ${nameplus}
+done
+
+# This script quality trims and removes adapters from raw reads using the program fastp.
