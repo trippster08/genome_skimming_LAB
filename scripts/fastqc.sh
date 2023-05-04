86ba40d157243067fefee645d6378d9606324638
Tripp Macdonald <trippster08@outlook.com>
2023-05-04 16:02:14 -0400

    Revamp of fastqc shell script
    This is a wholesale change of the script for quality evaluation using fastQC. This single script can now examine both raw and trimmed reads, depending upon the input parameters

--- scripts/fastqc.sh
+++ scripts/fastqc.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
+raw="$1"
+mkdir -p ${raw}/fastqc_analyses
 
-path="$1"
-mkdir ${path}/fastqc_analyses
+ls ${raw}/*.fastq.gz &> /dev/null  || echo "Correct path to read files not entered (*.fastq.gz)"
 
- for x in ${path}/*.fastq.gz ; do 
-  sampleplus=`echo ${x} | cut -d "_" -f 1,2,3,4,5 | cut -d "/" -f 8`
-  sample=`echo ${sampleplus} | cut -d "_" -f 1,2`
+for x in ${raw}/*.fastq.gz ; do 
+  sample=`basename ${x}`
+  name=`echo ${sample%_001*}`
 
-  qsub -o logs/${sample}_fastqc.log \
-  -N ${sample}_fastqc \
-  fastqc_multi.job ${path} ${sampleplus}
-
+  qsub -o logs/${name}_fastqc.log \
+  -N ${name}_fastqc \
+  fastqc.job ${raw} ${sample}
 done
 
-# This script runs a fastqc analysis on multiple fastq sequences, usually the
-# output of a miseq/hiseq/novaseq run. It assumes the file names are the typical
-# demultiplexed sample names downloaded by basespace. All files should be in the
-# same folder usually PROJECT/data/raw. This file should be in PROJECT/jobs.
-# For each sample file, it will output an html file that you can open with your
-# browser and view visual represntations of your sample quality.
-# It will also output a .zip file that contains text summaries of read quality.
+
+
+# This script runs a fastqc analysis on multiple fastq sequences, either raw
+# demultiplexed sequences or trimmed sequences that that have been output using
+# either trimmomatic or fastp. It assumes the raw file names are
+# in the format output by Illumina (for example 
+# "SamplesName_Barcode_L001_R1_001.fastq.gz"), and the trimmed sequences are
+# in the format used in this pipeline as the output from trimmomatic or fastp
+# (for example: "SampleName_Barcode_R1_PE_trimmed.fastq.gz")
+# Raw sequences should be in PROJECT/data/raw/ and trimmed sequences should be in
+# PROJECT/data/trimmed_sequences/. For each sample file, it will output an html
+# file that you should transfer to your local computer. Open this file with your
+# browser and view the visual representations of your sample quality.
+# It will also output a .zip file that contains text summaries of read quality.  
+# See https://confluence.si.edu/pages/viewpage.action?pageId=163152227 for help
+# with transferring files between Hydra and your computer.
