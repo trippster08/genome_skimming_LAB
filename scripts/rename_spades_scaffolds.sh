760d51f89f3d6131efaa9acaea5a536467964271
trippster08 <trippster08@outlook.com>
2023-06-27 23:45:25 -0400

    Added error-corrected reads I added to this shell script the creation of a directory for read-corrected reads, and copied the reads to this directory

--- scripts/rename_spades_scaffolds.sh
+++ scripts/rename_spades_scaffolds.sh
@@ -3,10 +3,11 @@
 results="$1"
 cd ${results}
 mkdir ../spades_scaffolds
-
+mkdir ../error_corrected_reads
 for x in ${results}/*; do 
   sample=`basename ${x}`
   ls ${sample}/scaffolds.fasta &> /dev/null  || echo "Correct path to SPAdes results not entered"
   cp ${sample}/scaffolds.fasta ${results}/../spades_scaffolds/${sample}_spades_scaffolds.fasta
   cp ${sample}/spades.log ${results}/../spades_scaffolds/${sample}_spades.log
+  cp ${sample}/corrected/* ${results}/../error_corrected_reads/
 done
