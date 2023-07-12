27b7086999b8c99f3aba0e8b831add1884e1b396
Tripp Macdonald <trippster08@outlook.com>
2023-07-12 14:25:23 -0400

    Added GetOrganelle shell script
    This script creates a job file to run GetOrganelle for each sample to find Mitochondrial or Chloroplast genomes

---
+++ scripts/getorganelle.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+trimmed="$1"
+organelle="$2"
+data=${trimmed}/../
+
+mkdir -p ${data}/results/getorganelle/
+
+ for x in ${trimmed}/*_R1_PE_trimmed.fastq.gz ; do 
+  sample=`basename ${x}`
+  name=`echo ${sample%_R*}`
+
+    qsub -o ${data}/results/getorganelle/${name}_getorganelle.log \
+    -N ${name}_getorganelle \
+    getorganelle_multi.job ${trimmed} ${organelle} ${name} ${data}
+
+done
+
+# This GetOrganelle shell requires two elements after calling the script
+# 1: path to the trimmed sequences
+# 2: the organelle you are trying to find. 
+# Organelle list to use for the 2nd element passed in calling the shell script:
+#   embplant_pt
+#   other_pt
+#   embplant_mt
+#   embplant_nr
+#   animal_mt
+#   fungus_mt
