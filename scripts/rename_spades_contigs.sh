f6bfb8b1bd64b5a09e3acca63724e10a0fff45c3
trippster08 <trippster08@outlook.com>
2023-08-07 11:59:13 -0400

    changed log file placement

--- scripts/rename_spades_contigs.sh
+++ scripts/rename_spades_contigs.sh
@@ -8,6 +8,6 @@ for x in ${results}/*; do
   sample=`basename ${x}`
   ls ${sample}/contigs.fasta &> /dev/null  || echo "Correct path to SPAdes results not entered"
   cp ${sample}/contigs.fasta ${results}/../spades_contigs/${sample}_spades_contigs.fasta
-  cp ${sample}/spades.log ${results}/../spades_contigs/${sample}_spades.log
+  cp ${sample}_spades.log ${results}/../spades_contigs/${sample}_spades.log
   cp ${sample}/corrected/* ${results}/../error_corrected_reads/
 done
