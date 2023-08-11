bb244552b1c310ba77fc5493a873735ace88bef7
trippster08 <trippster08@outlook.com>
2023-08-11 00:27:51 -0400

    added MITOS job and shell files

--- scripts/mitofinder_annotate_spades.sh
+++ scripts/mitofinder_annotate_spades.sh
@@ -22,6 +22,7 @@ for x in ${contigs}/*_spades_contigs.fasta ; do
     mitofinder_annotate_spades.job ${contigs} ${name} ${taxa}
 done
 
+
 # This is different from a normal mitofinder run because it doesn't use the raw
 # sequences as an input, but instead uses the contigs.fasta output from a
 # spades assembly. This is signficantly faster, and I have found that spades 
@@ -32,3 +33,28 @@ done
 # tells hydra to run the program in whatever directory is listed, in this case 
 # results/mitofinder_spades. Since this would also cause the log
 # files to be placed here, I have also changed the log output "-o" to jobs/logs/
+
+# This MitoFinder shell requires two elements after calling the script
+# 1: path to the spades contigs
+# 2: the genetic code
+
+# The shell expects on of the following Genetic codes
+# 1. The Standard Code 
+# 2. The Vertebrate Mitochondrial Code 
+# 3. The Yeast Mitochondrial Code 
+# 4. The Mold, Protozoan, and Coelenterate Mitochondrial Code and the
+#     Mycoplasma/Spiroplasma Code
+# 5. The Invertebrate Mitochondrial Code
+# 6. The Ciliate, Dasycladacean and Hexamita Nuclear Code 
+# 9. The Echinoderm and Flatworm Mitochondrial Code 
+# 10. The Euplotid Nuclear Code 
+# 11. The Bacterial, Archaeal and Plant Plastid Code 
+# 12. The Alternative Yeast Nuclear Code 
+# 13. The Ascidian Mitochondrial Code 
+# 14. The Alternative Flatworm Mitochondrial Code 
+# 16. Chlorophycean Mitochondrial Code 
+# 21. Trematode Mitochondrial Code 
+# 22. Scenedesmus obliquus Mitochondrial Code 
+# 23. Thraustochytrium Mitochondrial Code 
+# 24. Pterobranchia Mitochondrial Code 
+# 25. Candidate Division SR1 and Gracilibacteria Code
