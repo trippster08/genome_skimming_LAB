66986410b508e511a7d182618c6e96199bf16872
trippster08 <trippster08@outlook.com>
2023-08-21 15:05:19 -0400

    added spades to error corrected reads
    Added "spades" to error-corrected reads directory

--- scripts/rename_spades_contigs.sh
+++ scripts/rename_spades_contigs.sh
@@ -3,11 +3,11 @@
 results="$1"
 cd ${results}
 mkdir ../spades_contigs
-mkdir ../error_corrected_reads
+mkdir ../spades_error_corrected_reads
 for x in ${results}/*; do 
   sample=`basename ${x}`
   ls ${sample}/contigs.fasta &> /dev/null  || echo "Correct path to SPAdes results not entered"
   cp ${sample}/contigs.fasta ${results}/../spades_contigs/${sample}_spades_contigs.fasta
   cp ${sample}_spades.log ${results}/../spades_contigs/${sample}_spades.log
-  cp ${sample}/corrected/* ${results}/../error_corrected_reads/
+  cp ${sample}/corrected/* ${results}/../spades_error_corrected_reads/
 done
