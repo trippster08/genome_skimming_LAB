dd23851eeb2902a9972501c9866779ced3180060
trippster08 <trippster08@outlook.com>
2023-08-24 17:52:51 -0400

    Improved conditional statements
    Expanded the conditions to give error statements and then exit the program if variables are entered incorrectly

--- scripts/spades.sh
+++ scripts/spades.sh
@@ -2,9 +2,15 @@
 
 trimmed="$1"
 data=${trimmed}/../
-mkdir -p ${data}/results/spades
 
-ls ${trimmed}/*.fastq.gz &> /dev/null  || echo "Correct path to read files not entered (*.fastq.gz)"
+if
+  ls ${trimmed}/*.fastq.gz &> /dev/null 
+then
+  echo "Correct path to read files not entered (*.fastq.gz)";
+  exit 1;
+fi
+
+mkdir -p ${data}/results/spades
 
 for x in ${trimmed}/*_R1_PE_trimmed.fastq.gz ; do 
   sample=`basename ${x}`
