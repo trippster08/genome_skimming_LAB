29c902d4d2b07a0611bfead1d6b4b3f0c47dd61e
trippster08 <trippster08@outlook.com>
2023-08-31 14:12:26 -0400

    changed parts of the for loop

--- scripts/rename_spades_contigs.sh
+++ scripts/rename_spades_contigs.sh
@@ -4,10 +4,10 @@ results="$1"
 cd ${results}
 mkdir ../spades_contigs
 mkdir ../spades_error_corrected_reads
-for x in ${results}/*; do 
+for x in ${results}/*/; do 
   sample=`basename ${x}`
   ls ${sample}/contigs.fasta &> /dev/null  || echo "Correct path to SPAdes results not entered"
   cp ${sample}/contigs.fasta ${results}/../spades_contigs/${sample}_spades_contigs.fasta
   cp ${sample}_spades.log ${results}/../spades_contigs/${sample}_spades.log
-  cp ${sample}/corrected/* ${results}/../spades_error_corrected_reads/
+  cp ${sample}/corrected/${sample}* ${results}/../spades_error_corrected_reads/
 done
