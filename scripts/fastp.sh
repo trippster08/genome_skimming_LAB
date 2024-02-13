60e92b69f2c6c1eaea058972cdb89dcd0e905aaa
trippster08 <trippster08@outlook.com>
2024-02-13 13:33:26 -0600

    changed name of file for loop
    I changed the name requirements for input raw files to only include a sample name and Rx, and I added a new variable for the job. Also, fixed a misspelling in the job

--- scripts/fastp.sh
+++ scripts/fastp.sh
@@ -12,17 +12,18 @@ fi
 
 mkdir -p ${data}/trimmed_sequences
 
-for x in ${raw}/*R1*.fastq.gz ; do 
+for x in ${raw}/*_R1* ; do 
   sample=`basename ${x}`
   name=`echo ${sample%%_*}`
   nameplus=`echo ${sample%_R*}`
+  post=`echo ${sample#*_R[1-2]*}`
   # echo $sample
   # echo $name
   # echo $nameplus
 
     qsub -o logs/${name}_fastp.log \
     -N ${name}_fastp \
-    fastp.job ${raw} ${data} ${name} ${nameplus}
+    fastp.job ${raw} ${data} ${name} ${nameplus} ${post}
 done
 
 # This script quality trims and removes adapters from raw reads using the program fastp. See .job file for parameter directions
