4a1cb066b49370ff508f9a945cfff96992a947aa
trippster08 <trippster08@outlook.com>
2024-02-22 17:42:56 -0500

    Added shell script for convert SRA
    added shell script to start job to use fasterq-dump to convert SRA data to fastq data, and use gzip to convert fastq data to fastq.gz

---
+++ extra_scripts/convert_sra.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+sra="$1"
+data=${sra}/../
+
+
+for x in ${sra}/*/ ; do 
+  sample=`basename ${x}`
+
+if
+  [[ -z "$(ls ${sra}/${sample}/*.sra 2>/dev/null | grep sra)" ]]  
+then  
+  echo "Correct path to sra file not entered (*.sra)"
+  exit
+fi
+
+    qsub -o logs/${sample}_fasterq-dump.log \
+    -N ${sample}_fasterq-dump \
+    convert_sra.job ${sra} ${data} ${sample}
+done
+
+# This script quality trims and removes adapters from raw reads using the program fastp. See .job file for parameter directions
