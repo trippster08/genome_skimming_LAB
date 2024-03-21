6e896ffc8a68cf9b0db5e7d17694acf25b347c09
trippster08 <trippster08@outlook.com>
2024-03-21 09:31:35 -0400

    fixed paths

--- scripts/copy_getorganelle_scaffolds.sh
+++ scripts/copy_getorganelle_scaffolds.sh
@@ -7,13 +7,15 @@ mkdir ../getorganelle_scaffolds
 
 for x in ${results}/*/; do 
   sample=`basename ${x}`
-  mkdir ../getorganelle_scaffolds/${sample}
-  for y in ${results}/${sample}/*_sequence.fasta; do
+  for y in ${results}/${sample}/*path_sequence.fasta; do
     sample2=`basename ${y}`
-    name=`echo ${sample2%_sequence*}`
-  cp ${sample}/${sample2} ../getorganelle_scaffolds/${sample}/${sample}_${sample2}
-  cp ${sample}_getorganelle.log ../getorganelle_scaffolds/${sample}/${sample}.log
+    name=`echo ${sample2%_path_sequence.fasta}`
+  cp ${sample}/${sample2} ../getorganelle_scaffolds/${sample}_${sample2}
+  cp ${sample}_getorganelle.log ../getorganelle_scaffolds/${sample}.log
   done
 done
 
-# This makes a new folder called "getorganelle_scaffolds", and copies all scaffolds from each getorganelle results folder. Since GetOrganelle names it's scaffolds generically, this script also renames them by adding the sample name to the beginning of each
+# This makes a new folder called "getorganelle_scaffolds", and copies all
+# scaffolds from each getorganelle results folder. Since GetOrganelle names
+# it's scaffolds generically, this script also renames them by adding the
+# sample name to the beginning of each
