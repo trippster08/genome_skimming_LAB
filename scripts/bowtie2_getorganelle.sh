591dbb95d9dad23e1ea322edd1924b80bebbd2f1
trippster08 <trippster08@outlook.com>
2024-04-11 10:06:19 -0400

    many changes to make it work with looping

--- scripts/bowtie2_getorganelle.sh
+++ scripts/bowtie2_getorganelle.sh
@@ -5,21 +5,30 @@ trimmed="$2"
 results=${contigs}/../
 
 if
-  [[ -z "$(ls ${contigs}/*_sequence.fasta 2>/dev/null | grep fasta)" ]] 
+  [[ -z "$(ls ${contigs}/*.path_sequence.fasta 2>/dev/null | grep fasta)" ]] 
 then
-  echo "Correct path to SPAdes results not entered (*_spades_contigs.fasta)"
+  echo "Correct path to GetOrganelle results not entered (*.path_sequence.fasta)"
+  exit
+fi
+
+if
+  [[ -z "$(ls ${trimmed}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]  
+then
+  echo "Correct path to trimmed read files not entered (*.fastq.gz)"
   exit
 fi
 
 mkdir -p ${results}/bowtie2_getorganelle
+mkdir -p ${results}/bowtie2_results
 
-for x in ${contigs}/*_path_sequence.fasta ; do 
+for x in ${contigs}/*.path_sequence.fasta ; do 
     sample=`basename ${x}`
     name=`echo ${sample%.path_sequence.fasta}`
-echo ${name}
-echo ${sample}
-    qsub -o logs/${name}_bowtie2_getorganelle.log \
+    shortname=${name%%_*}
+mkdir ${results}/bowtie2_getorganelle/${shortname}
+
+    qsub -o ${results}/../../jobs/logs/${shortname}_bowtie2_getorganelle.log \
     -wd ${results}/bowtie2_getorganelle \
-    -N ${name}_bowtie2_getorganelle \
-    bowtie2_getorganelle.job ${contigs} ${name} ${sample} ${trimmed} ${results}
+    -N ${shortname}_bowtie2_getorganelle \
+    bowtie2_getorganelle.job ${contigs} ${name} ${sample} ${trimmed} ${results} ${shortname}
 done
