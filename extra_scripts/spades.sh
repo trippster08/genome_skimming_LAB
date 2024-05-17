4b4a8be05e9709c92db4baee21b0be93ee8bffd3
trippster08 <trippster08@outlook.com>
2024-05-17 09:47:52 -0400

    Fixed spades shell and job
    Decreased memory, changed queue, simplified paths, fixed copying of results, removed incomplete error text file

--- extra_scripts/spades.sh
+++ extra_scripts/spades.sh
@@ -1,31 +1,26 @@
 #!/bin/sh
 
 trimmed="$1"
-data=${trimmed}/../
+results=${trimmed}/../results
 
 
 if
-  [[ -z "$(ls ${trimmed}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]  
+  [[ -z "$(ls ${trimmed}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]
 then
   echo "Correct path to read files not entered (*.fastq.gz)"
   exit
 fi
 
-mkdir -p ${data}/results/spades ${data}/results/spades_contigs
-echo "SPAdes did not sucessfully assemble the following samples. Please check the  \
-log to evaluate any problems. If you cannot get SPAdes to work on these samples, \
-please see LAB staff."
+mkdir -p ${results}/spades ${results}/spades_contigs
 
-results=${data}/${results}
-
-for x in ${trimmed}/*_R1_PE_trimmed.fastq.gz ; do 
+for x in ${trimmed}/*_R1_PE_trimmed.fastq.gz ; do
   sample=`basename ${x}`
-  name=`echo ${sample%_R*}`
+  name=`echo ${sample%%_*}`
 #  echo $sample
 #  echo $name
-  
+
     qsub -o logs/${name}_spades.log \
     -N ${name}_spades \
-    spades_loop.job ${trimmed} ${name} ${data} ${results}
-    
+    spades_loop.job ${trimmed} ${name} ${results}
+
 done
