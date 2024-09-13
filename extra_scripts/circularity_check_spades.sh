9641abeeda9cc13cc36917c47a0db9af0f32c0cf
trippster08 <trippster08@outlook.com>
2024-09-13 14:08:42 -0400

    added new sh and job for checking spades contigs for circularity
    I've added a new shell script and job file for checking circularity in spades contigs. It takes all contigs from a fasta file and searches to see if the first 50 bp of a sequence can be found anywhere in the last 200 bp of that sequence, and outputs all sequences in which this is true into a new fasta file

---
+++ extra_scripts/circularity_check_spades.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+contigs="$1"
+results=${contigs}/../
+
+if
+  [[ -z "$(ls ${contigs}/*_spades_contigs.fasta 2>/dev/null | grep fasta)" ]] 
+then
+  echo "Correct path to SPAdes results not entered (*_spades_contigs.fasta)"
+  exit
+fi
+
+mkdir -p ${results}/possible_mitogenomes
+
+for x in ${contigs}/*_spades_contigs.fasta ; do 
+    sample=`basename ${x}`
+    name=`echo ${sample%_spades_contigs.fasta}`
+    qsub -o ${results}/../../jobs/logs/${name}_mitofinder_spades.log \
+    -wd ${results}/mitofinder_spades \
+    -N ${name}_mitofinder_spades \
+    mitofinder_annotate_spades_loop.job ${contigs} ${name} ${sample} ${results}
+
+  sleep 0.1
+done
+
+
