0e4f8fb05ec265864072521ad0b16ffd6be35d26
trippster08 <trippster08@outlook.com>
2025-01-06 16:25:26 -0500

    fixed variables and some arguments

--- extra_scripts/Nanopore/flye.sh
+++ extra_scripts/Nanopore/flye.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-raw="$1"
-data=${raw}/../
+trimmed_nanopore="$1"
+data=${trimmed_nanopore}/../
 
 if
-  [[ -z "$(ls ${raw}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]
+  [[ -z "$(ls ${trimmed_nanopore}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]
 then
   echo "Correct path to read files not entered (*.fastq.gz)"
   exit
@@ -14,13 +14,13 @@ fi
 mkdir -p ${data}/results/flye ${data}/results/flye_assemblies 
 results=${data}/results/
 
- for x in ${raw}/*.fastq.gz ; do 
+ for x in ${trimmed_nanopore}/*_trimmed.fastq.gz ; do 
   sample=`basename ${x}`
   name=`echo ${sample%.fastq.gz}`
   mkdir ${results}/flye/${name}
   qsub -o logs/${name}_flye_hydra.log \
   -N ${name}_flye \
-  flye_loop.job ${raw} ${sample} ${name} ${results}
+  flye_loop.job ${trimmed_nanopore} ${sample} ${name} ${results}
 
   #sleep 0.1
 done
