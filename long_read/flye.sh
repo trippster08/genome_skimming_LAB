5b5213ad76e613b0533eeb964f7042bc3e994422
trippster08 <trippster08@outlook.com>
2025-05-23 09:18:20 -0400

    new files for long-read data

---
+++ long_read/flye.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+raw="$1"
+data=${raw}/../
+
+if
+  [[ -z "$(ls ${raw}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]
+then
+  echo "Correct path to read files not entered (*.fastq.gz)"
+  exit
+fi
+
+
+mkdir -p ${data}/results/flye ${data}/results/flye_assemblies 
+results=${data}/results/
+
+ for x in ${raw}/*.fastq.gz ; do 
+  sample=`basename ${x}`
+  name=`echo ${sample%.fastq.gz}`
+  mkdir ${results}/flye/${name}
+  qsub -o logs/${name}_flye.log \
+  -N ${name}_flye \
+  flye_loop.job ${raw} ${sample} ${name} ${results}
+
+  #sleep 0.1
+done
