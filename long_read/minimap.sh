e90243a17545f010c0eb7081d9c121945e7647fe
trippster08 <trippster08@outlook.com>
2025-05-28 16:39:38 -0400

    added mapping to assembly using mimimap

---
+++ long_read/minimap.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+contigs="$1"
+raw_nanopore="$2"
+results=${contigs}/../
+
+if
+  [[ -z "$(find ${contigs}/ -name '*_contig.fasta' 2>/dev/null | grep fasta)" ]] 
+then
+  echo "Correct path to mitofinder contig not entered (*_mtDNA_contig.fasta)"
+  exit
+fi
+
+if
+  [[ -z "$(ls ${raw_nanopore}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]
+then
+  echo "Correct path to raw nanopore read files not entered (*.fastq.gz)"
+  exit
+fi
+
+mkdir -p ${results}/minimap_results
+
+
+ for x in ${contigs}/*/; do 
+  mitofinder_final_results=`basename ${x}`
+  name=`echo ${mitofinder_final_results%_mitofinder_flye_Final_Results}`
+  qsub -o logs/${name}_minimap_hydra.log \
+  -N ${name}_minimap \
+  minimap_loop.job ${contigs} ${raw_nanopore} ${name} ${results}
+
+  sleep 0.1
+done
