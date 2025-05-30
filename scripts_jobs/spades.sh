c1e98581362f4b6a0110f163ee9059856b793164
trippster08 <trippster08@outlook.com>
2025-05-30 14:36:49 -0400

    fixed if statement formatting

--- scripts_jobs/spades.sh
+++ scripts_jobs/spades.sh
@@ -4,9 +4,7 @@ trimmed="$1"
 results=${trimmed}/../results
 
 
-if
-  [[ -z "$(ls ${trimmed}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]
-then
+if [[ -z "$(ls ${trimmed}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]; then
   echo "Correct path to read files not entered (*.fastq.gz)"
   exit
 fi
