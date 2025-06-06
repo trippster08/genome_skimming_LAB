3512814900d3731b99859b505d7008d9869ac8ce
trippster08 <trippster08@outlook.com>
2025-06-06 15:08:30 -0400

    fixed check of previous successful run

--- long_read/flye.sh
+++ long_read/flye.sh
@@ -12,21 +12,28 @@ fi
 mkdir -p ${data}/results/flye ${data}/results/flye_assemblies 
 results=${data}/results/
 
- for x in ${raw}/*.fastq.gz ; do 
+for x in ${raw}/*.fastq.gz ; do 
   sample=`basename ${x}`
   name=`echo ${sample%.fastq.gz}`
-  if [ -f ${results}/flye_assemblies/${name}_flye_assembly.fasta ]; then
+  if [ -f ${results}/flye_assemblies/${name}_flye.log ]; then
     echo "Flye assemblies for ${name} have already been completed"
   elif [ -f logs/${name}_flye_hydra.log ]; then
     if [ -d ${results}/flye/${name} ]; then
       if [ -f ${results}/flye_assemblies/${name}_flye_hydra.log ]; then
-      rm -r ${results}/flye/${name} ${results}/flye_assemblies/${name}_flye_hydra.log logs/${name}_flye_hydra.log
-      mkdir ${results}/flye/${name}
-      qsub -o logs/${name}_flye_hydra.log \
-      -N ${name}_flye \
-      flye_loop.job ${raw} ${sample} ${name} ${results}    
+        rm -r ${results}/flye/${name} ${results}/flye_assemblies/${name}_flye_hydra.log logs/${name}_flye_hydra.log
+        mkdir ${results}/flye/${name}
+        qsub -o logs/${name}_flye_hydra.log \
+        -N ${name}_flye \
+        flye_loop.job ${raw} ${sample} ${name} ${results}
+      else
+        rm -r ${results}/flye/${name} logs/${name}_flye_hydra.log
+        mkdir ${results}/flye/${name}
+        qsub -o logs/${name}_flye_hydra.log \
+        -N ${name}_flye \
+        flye_loop.job ${raw} ${sample} ${name} ${results}
+      fi          
     else
-      rm ${results}/flye_assemblies/${name}_flye_hydra.log logs/${name}_flye_hydra.log
+      rm logs/${name}_flye_hydra.log
       mkdir ${results}/flye/${name}
       qsub -o logs/${name}_flye_hydra.log \
       -N ${name}_flye \
