6f2dd87418835d9ecdd07d88e6ac8ea7fc773fb2
trippster08 <trippster08@outlook.com>
2025-07-23 16:12:34 -0400

    updated to work with filtered and trimmed reads

--- long_read/flye.sh
+++ long_read/flye.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-raw="$1"
-data=${raw}/../
+reads="$1"
+data=${reads}/../
 
-if [[ -z "$(ls ${raw}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]; then
+if [[ -z "$(ls ${reads}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]; then
   echo "Correct path to read files not entered (*.fastq.gz)"
   exit
 fi
@@ -12,9 +12,9 @@ fi
 mkdir -p ${data}/results/flye ${data}/results/flye_assemblies 
 results=${data}/results/
 
-for x in ${raw}/*.fastq.gz ; do 
+for x in ${reads}/*.fastq.gz ; do 
   sample=`basename ${x}`
-  name=`echo ${sample%.fastq.gz}`
+  name=`echo ${sample%_trimmed_filtered.fastq.gz}`
   if [ -f ${results}/flye_assemblies/${name}_flye.log ]; then
     echo "Flye assemblies for ${name} have already been completed"
   elif [ -f logs/${name}_flye_hydra.log ]; then
@@ -24,26 +24,26 @@ for x in ${raw}/*.fastq.gz ; do
         mkdir ${results}/flye/${name}
         qsub -o logs/${name}_flye_hydra.log \
         -N ${name}_flye \
-        flye_loop.job ${raw} ${sample} ${name} ${results}
+        flye_loop.job ${reads} ${sample} ${name} ${results}
       else
         rm -r ${results}/flye/${name} logs/${name}_flye_hydra.log
         mkdir ${results}/flye/${name}
         qsub -o logs/${name}_flye_hydra.log \
         -N ${name}_flye \
-        flye_loop.job ${raw} ${sample} ${name} ${results}
+        flye_loop.job ${reads} ${sample} ${name} ${results}
       fi          
     else
       rm logs/${name}_flye_hydra.log
       mkdir ${results}/flye/${name}
       qsub -o logs/${name}_flye_hydra.log \
       -N ${name}_flye \
-      flye_loop.job ${raw} ${sample} ${name} ${results}
+      flye_loop.job ${reads} ${sample} ${name} ${results}
     fi
   else
     mkdir ${results}/flye/${name}
     qsub -o logs/${name}_flye_hydra.log \
     -N ${name}_flye \
-    flye_loop.job ${raw} ${sample} ${name} ${results}
+    flye_loop.job ${reads} ${sample} ${name} ${results}
   fi
   #sleep 0.1
 done
