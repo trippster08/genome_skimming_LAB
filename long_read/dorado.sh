adacfaa8e44879c60f7cffcd5727ef5d9587146b
trippster08 <trippster08@outlook.com>
2025-07-23 15:58:29 -0400

    created job and shell script for dorado
    Dorado trims barcodes, primers, and adapters from nanopore runs

---
+++ long_read/dorado.sh
@@ -0,0 +1,30 @@
+ #!/bin/sh
+
+raw="$1"
+data=${raw}/../
+
+if [[ -z "$(ls ${raw}/*.fastq.gz 2>/dev/null | grep fastq.gz)" ]]; then
+  echo "Correct path to read files not entered (*.fastq.gz)"
+  exit
+fi
+
+
+mkdir -p ${data}/trimmed_reads/ 
+
+for x in ${raw}/*.fastq.gz ; do 
+  sample=`basename ${x}`
+  name=`echo ${sample%.fastq.gz}`
+  if [ -f ${trimmed_reads}/${name}_trimmed.fastq.gz ]; then
+    echo "Primer/adapter trimming for ${name} has already been completed"
+  elif [ -f logs/${name}_dorado_hydra.log ]; then
+    rm -r logs/${name}_dorado_hydra.log
+    qsub -o logs/${name}_dorado_hydra.log \
+    -N ${name}_dorado \
+    dorado_loop.job ${raw} ${sample} ${name} ${data}
+  else
+    qsub -o logs/${name}_dorado_hydra.log \
+    -N ${name}_dorado \
+    dorado_loop.job ${raw} ${sample} ${name} ${data}
+  fi
+  sleep 0.1
+done
