72de42010706910305a77dd517982f7e1c427e02
trippster08 <trippster08@outlook.com>
2025-08-21 12:17:35 -0400

    moved creation of raw read and config file to job from shell script

--- long_read/nextpolish.sh
+++ long_read/nextpolish.sh
@@ -27,31 +27,9 @@ for x in ${trimmed_illumina_reads}/*R1_PE_trimmed.fastq.gz ; do
   if [ -f ${results}/nextpolished_assemblies/${name}_genome_nextpolished.fasta ]; then
     echo "Assemblies for ${name} have already benn polished by nextPolish"
   else
-    # Create .fofn file
-    echo ${trimmed_illumina_reads}/${name}_R1_PE_trimmed.fastq.gz > ${results}/nextpolish/${name}.fofn
-    echo ${trimmed_illumina_reads}/${name}_R2_PE_trimmed.fastq.gz >> ${results}/nextpolish/${name}.fofn
-    # Create run.cfg file
-    cat > "${results}/nextpolish/${name}_run.cfg" <<EOF
-[General]
-job_type = local
-job_prefix = nextPolish
-task = best
-rewrite = yes
-rerun = 3
-parallel_jobs = 6
-multithread_jobs = 5
-genome = ${medaka_corrected_assemblies}/${name}_medaka_consensus.fasta #genome file
-genome_size = auto
-workdir = ${results}/nextpolish/${name}
-polish_options = -p {multithread_jobs} -u -debug
-
-[sgs_option]
-sgs_fofn = ${results}/nextpolish/${name}.fofn
-sgs_options = -max_depth 100 -bwa
-EOF
     qsub -o logs/${name}_nextpolish_hydra.log \
     -N ${name}_nextpolish \
-    nextpolish_loop.job ${name} ${results}
+    nextpolish_loop.job ${name} ${results} ${trimmed_illumina_reads} ${medaka_corrected_assemblies}
   fi
   sleep 0.1
 done
